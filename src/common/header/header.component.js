cd65df0c7ad88a19fc40f27f1d3b1dff78013716
Pravin-Parkhi <pravinparkhi.pp@gmail.com>
2020-09-19 16:03:54 +0530

    fix style issues

--- src/common/header/header.component.js
+++ src/common/header/header.component.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import brandLogo from '../../assets/generic-logo-hi.png'
+
 import './header.component.scss'
 
 export default function Header(props) {
 
   return (
     <div className='header-wrapper'>
-
+      <img src={brandLogo} alt='Company Logo' />
     </div>
   );
 }
